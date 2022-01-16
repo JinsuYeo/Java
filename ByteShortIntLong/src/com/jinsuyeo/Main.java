477fcfe57cc425c6234621e878ecdbd5aecdfbd0
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-17 01:05:55 +0900

    java byte, short, long type
    java에서 long타입에 리터럴 상수로 할당 시 끝에 l or L 붙여주어야 함. 안그러면 int 타입으로 인식 됨. but int타입의 사이즈보다 크지 않은 상수는 컴파일러가 알아서 변환해 줌. 반대로 long보다는 작지만 int보다 큰 리터럴 상수일 때 l붙이지 않으면 오류.

--- ByteShortIntLong/src/com/jinsuyeo/Main.java
+++ ByteShortIntLong/src/com/jinsuyeo/Main.java
@@ -7,10 +7,26 @@ public class Main {
 
         int myMinIntValue = Integer.MIN_VALUE;
         int myMaxIntValue = Integer.MAX_VALUE;
-
         System.out.println("Integer Minimum Value = " + myMinIntValue);
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        
+        int myMaxValueTest = 2_147_483_647;
+        System.out.println(myMaxValueTest);
 
-        int myMaxValueTest = 2147483647;
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
     }
 }
