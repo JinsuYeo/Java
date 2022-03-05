65acfc49767df4ec70ea63a15245d5e6dd5c4c64
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-03-06 02:57:59 +0900

    추가 미션2 수정
    힌트에 나와있는 방법으로 입력받은 문자열을 두개의 int로 나누는 방법 수정.

--- Java-Playground/Gugudan/src/GugudanMain.java
+++ Java-Playground/Gugudan/src/GugudanMain.java
@@ -1,8 +1,9 @@
 public class GugudanMain {
 	public static void main(String[] args) {
 		String dan = Gugudan.inputDan();
-		int first = dan.charAt(0) - '0'; // "8,7"과 같은 문자열이 입력됨.
-		int second = dan.charAt(2) - '0';
+		String[] temp = dan.split(","); // "8,7"과 같은 문자열이 입력됨.
+		int first = Integer.parseInt(temp[0]); 
+		int second = Integer.parseInt(temp[1]);
 		Gugudan.calc(first, second);
 	}
 }
