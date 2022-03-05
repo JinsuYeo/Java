9537a6dde819005bd693568a235488bcace5fcf8
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-03-06 02:40:57 +0900

    추가 미션1
    자바 플레이그라운드 구구단 추가미션 1.
    사용자 입력받아 2*1 ~ n*n단 까지 출력.

--- Java-Playground/Gugudan/src/Gugudan.java
+++ Java-Playground/Gugudan/src/Gugudan.java
@@ -1,8 +1,20 @@
+import java.util.*;
+
 public class Gugudan {
+	public static int inputDan() {
+		Scanner scanner = new Scanner(System.in);
+		int dan = scanner.nextInt();
+		
+		return dan;
+	}
+	
 	public static int[] calc(int dan) {
-		int[] result = new int[9];
-		for(int i = 0; i < result.length; i++) {
-			result[i] = dan * (i+1);
+		int[] result = new int[dan];
+		for(int i = 2; i <= result.length; i++) {
+			for(int j = 0; j < result.length; j++) {
+				result[j] = i * (j+1);
+			}
+			print(result);
 		}
 		return result;
 	}
--- Java-Playground/Gugudan/src/GugudanMain.java
+++ Java-Playground/Gugudan/src/GugudanMain.java
@@ -1,9 +1,7 @@
 public class GugudanMain {
 	public static void main(String[] args) {
-		for(int i = 2; i <= 9; i++) {
-			int[] result = Gugudan.calc(i);
-			System.out.print(i + "단: ");
-			Gugudan.print(result);
-		}
+		int dan = Gugudan.inputDan();
+		int[] result = Gugudan.calc(dan);
+		Gugudan.print(result);
 	}
 }
