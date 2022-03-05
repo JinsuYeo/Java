92eb10bbbf4c321251d874b0f9a63a1399282e23
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-03-06 02:17:25 +0900

    자바 메서드
    메서드 이용해 구구단 구현.

--- Java-Playground/Gugudan/src/Gugudan.java
+++ Java-Playground/Gugudan/src/Gugudan.java
@@ -1,15 +1,24 @@
 public class Gugudan {
+	public static int[] calc(int dan) {
+		int[] result = new int[9];
+		for(int i = 0; i < result.length; i++) {
+			result[i] = dan * (i+1);
+		}
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int j = 0; j < result.length; j++) {
+			System.out.print(result[j] + " ");
+		}
+		System.out.println("\n");
+	}
+	
 	public static void main(String[] args) {
-		for(int i = 1; i <= 9; i++) {
-			int[] result = new int[9];
-			for(int j = 0; j < result.length; j++) {
-				result[j] = i * (j+1); 
-			}
+		for(int i = 2; i <= 9; i++) {
+			int[] result = calc(i);
 			System.out.print(i + "단: ");
-			for(int j = 0; j < result.length; j++) {
-				System.out.print(result[j] + " ");
-			}
-			System.out.println("\n");
+			print(result);
 		}
 	}
 }
