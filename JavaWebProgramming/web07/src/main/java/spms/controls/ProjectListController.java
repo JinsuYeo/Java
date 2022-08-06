be3082afa830810c21aeeab104a9fe111150c552
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-08-06 17:55:21 +0900

    자바 웹개발 워크북(07 퍼시스턴스 프레임워크의 도입)
    7. 동적 SQL 학습 및 적용(프로젝트 목록 정렬 기능 구현).

--- JavaWebProgramming/web07/src/main/java/spms/controls/ProjectListController.java
+++ JavaWebProgramming/web07/src/main/java/spms/controls/ProjectListController.java
@@ -1,23 +1,34 @@
 package spms.controls;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import spms.annotation.Component;
+import spms.bind.DataBinding;
 import spms.controls.Controller;
 import spms.dao.ProjectDao;
 
 @Component("/project/list.do")
-public class ProjectListController implements Controller {
+public class ProjectListController implements Controller, DataBinding {
   ProjectDao projectDao;
   
   public ProjectListController setMemberDao(ProjectDao projectDao) {
     this.projectDao = projectDao;
     return this;
   }
+  
+  public Object[] getDataBinders() {
+	  return new Object[] {
+		"orderCond", String.class
+	  };
+  }
 
   @Override
   public String execute(Map<String, Object> model) throws Exception {
-    model.put("projects", projectDao.selectList());
+	HashMap<String, Object> paramMap = new HashMap<>();
+	paramMap.put("orderCond", model.get("orderCond"));
+	
+    model.put("projects", projectDao.selectList(paramMap));
     return "/project/ProjectList.jsp";
   }
 }
