373bd7d90da14bd280be4c90bb55776a36c34b6e
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-08-15 03:07:40 +0900

    자바 웹개발 워크북(07 퍼시스턴스 프레임워크의 도입)
    07 퍼시스턴스 프레임워크의 도입
    
      1. 실력향상훈련: 회원 관리에 mybatis 적용.
      2. 정리보며 복습.

--- JavaWebProgramming/web07/src/main/java/spms/controls/MemberListController.java
+++ JavaWebProgramming/web07/src/main/java/spms/controls/MemberListController.java
@@ -1,14 +1,23 @@
 package spms.controls;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import spms.annotation.Component;
+import spms.bind.DataBinding;
 import spms.dao.MemberDao;
 
 // Annotation 적용
 @Component("/member/list.do")
-public class MemberListController implements Controller {
+public class MemberListController implements Controller, DataBinding {
   MemberDao memberDao;
+  
+  @Override
+  public Object[] getDataBinders() {
+  	return new Object[] {
+  			"orderCond", String.class
+  	};
+  }
   
   public MemberListController setMemberDao(MemberDao memberDao) {
     this.memberDao = memberDao;
@@ -17,7 +26,12 @@ public class MemberListController implements Controller {
 
   @Override
   public String execute(Map<String, Object> model) throws Exception {
-    model.put("members", memberDao.selectList());
+	HashMap<String, Object> paramMap = new HashMap<>();
+	paramMap.put("orderCond", model.get("orderCond"));
+	  
+    model.put("members", memberDao.selectList(paramMap));
     return "/member/MemberList.jsp";
   }
+
+
 }
