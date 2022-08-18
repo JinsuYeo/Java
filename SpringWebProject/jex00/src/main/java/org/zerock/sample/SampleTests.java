3d376abe842181fd546196a03ac7fe50069130d0
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-08-18 12:53:10 +0900

    코드로 배우는 스프링 웹 프로젝트 - 스프링 개발환경 구축
      1. 의존성 주입 테스트.
      2. 테스트 코드 이용해 스프링 동작 확인.

---
+++ SpringWebProject/jex00/src/main/java/org/zerock/sample/SampleTests.java
@@ -0,0 +1,34 @@
+package org.zerock.sample;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.config.RootConfig;
+
+import lombok.extern.log4j.Log4j;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= {RootConfig.class})
+@Log4j
+public class SampleTests {
+	
+	@Setter(onMethod_ = { @Autowired })
+	private Restaurant restaurant;
+	
+	@Test
+	public void testExist() {
+		
+		assertNotNull(restaurant);
+		
+		log.info(restaurant);
+		log.info("---------------------------");
+		log.info(restaurant.getChef());
+	}
+	
+}
